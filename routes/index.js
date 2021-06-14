7bf7c118266bfb231f47c96432f4689c8593f4df
Benarivo66 <iz4comed@yahoo.com>
2021-06-14 12:53:42 +0100

    just checking node_modules

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.send('Resume Backend Devs are the best in the whole wide world');
+	res.send('Resume Backend Devs are the best in the whole wide world, yeah', );
 });
 
 module.exports = router;
